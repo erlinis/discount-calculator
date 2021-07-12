fe372665e5ff19f38ce1da810a96b1d91f3131a0
Erlinis Quintana <erlinis.quintana@tourlane.com>
2021-07-12 21:52:47 +0200

    Updating dependencies and fix tailwind with new jit configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   plugins: {
-    '@tailwindcss/jit': { config: 'tailwind.config.js' },
+    tailwindcss: { config: 'tailwind.config.js' },
     autoprefixer: {},
     'postcss-import': {},
     // 'postcss-preset-env': {},
