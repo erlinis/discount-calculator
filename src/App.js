852b3a48aa90d98e4f9aeb80069d8efc3acfb5ff
Erlinis Quintana <erlinis.quintana@gmail.com>
2024-01-20 15:11:08 +0100

    refator: remove unused function

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useEffect, useRef, useState } from 'react';
-import AddHomePopup from './components/AddHomePopup';
-import Calculator from './components/Calculator';
-import DiscountList from './components/DiscountList';
-import { isInStandaloneMode, isIos } from './lib/appleDeviceDetector';
+import { useEffect, useRef, useState } from 'react'
+import AddHomePopup from './components/AddHomePopup'
+import Calculator from './components/Calculator'
+import DiscountList from './components/DiscountList'
+import { isInStandaloneMode, isIos } from './lib/appleDeviceDetector'
 
 function App() {
-  const priceInputRef = useRef();
+  const priceInputRef = useRef()
   const [state, setState] = useState(() => {
     return {
       discounts: getDiscountList(),
       showInstallMessage: isAppleDevice() && !getInstallMessage(),
-    };
-  });
+    }
+  })
 
   /**
    *
@@ -22,14 +22,7 @@ function App() {
    * @param {string} saving
    * @param {string} description
    */
-  function onAddDiscount(
-    price,
-    amount,
-    discount,
-    salePrice,
-    saving,
-    description
-  ) {
+  function onAddDiscount(price, amount, discount, salePrice, saving, description) {
     var discountRow = {
       id: Date.now(),
       price: Number(price),
@@ -38,14 +31,14 @@ function App() {
       saving: saving,
       salePrice: Number(salePrice),
       description: description,
-    };
+    }
 
     setState((prevState) => {
       return {
         ...prevState,
         discounts: [discountRow, ...prevState.discounts],
-      };
-    });
+      }
+    })
   }
 
   function onDeleteDiscount(itemId) {
@@ -53,96 +46,77 @@ function App() {
       return {
         ...prevState,
         discounts: prevState.discounts.filter((item) => item.id !== itemId),
-      };
-    });
+      }
+    })
   }
 
-  var discounts = state.discounts;
+  var discounts = state.discounts
 
   useEffect(() => {
-    storeDiscountList(discounts);
-  }, [discounts]);
-
-  function focusPriceInput() {
-    // Explicitly focus the text input using the raw DOM API
-    // Note: we're accessing "current" to get the DOM node
-    priceInputRef.current.focus();
-  }
+    storeDiscountList(discounts)
+  }, [discounts])
 
   function onPopupPress(e) {
     setState((prevState) => {
       return {
         ...prevState,
         showInstallMessage: false,
-      };
-    });
+      }
+    })
   }
 
   useEffect(() => {
     if (state.showInstallMessage === false) {
-      storeInstallMessage();
+      storeInstallMessage()
     }
-  });
+  })
 
   return (
     <>
       <header className="banner">
         <picture className="logo">
           <source srcSet="images/logo/ticket-logo-2x.png 2x, images/logo/ticket-logo-3x.png 3x" />
-          <img
-            src="images/logo/ticket-logo-1x.png"
-            alt="logo"
-            width="50"
-            height="32"
-          />
+          <img src="images/logo/ticket-logo-1x.png" alt="logo" width="50" height="32" />
         </picture>
         <h1 className="text-2xl font-bold">Discount Calculator</h1>
       </header>
       <div className="container-wrapper">
         <div className="">
-          <Calculator
-            priceInputRef={priceInputRef}
-            onAddDiscount={onAddDiscount}
-          />
-          <DiscountList
-            discounts={state.discounts}
-            onDeleteDiscount={onDeleteDiscount}
-          />
+          <Calculator priceInputRef={priceInputRef} onAddDiscount={onAddDiscount} />
+          <DiscountList discounts={state.discounts} onDeleteDiscount={onDeleteDiscount} />
         </div>
-        {state.showInstallMessage ? (
-          <AddHomePopup onPress={onPopupPress} />
-        ) : null}
+        {state.showInstallMessage ? <AddHomePopup onPress={onPopupPress} /> : null}
       </div>
     </>
-  );
+  )
 }
 
 function getInstallMessage() {
-  const item = localStorage.getItem('@discount/installMessage');
+  const item = localStorage.getItem('@discount/installMessage')
   if (item === 'true') {
-    return true;
+    return true
   }
-  return false;
+  return false
 }
 
 function storeInstallMessage() {
-  localStorage.setItem('@discount/installMessage', true);
+  localStorage.setItem('@discount/installMessage', true)
 }
 
 function isAppleDevice() {
-  return isIos() && !isInStandaloneMode();
+  return isIos() && !isInStandaloneMode()
 }
 
 function storeDiscountList(discounts) {
-  localStorage.setItem('@discount/discountList', JSON.stringify(discounts));
+  localStorage.setItem('@discount/discountList', JSON.stringify(discounts))
 }
 
 function getDiscountList() {
   try {
-    return JSON.parse(localStorage.getItem('@discount/discountList') || []);
+    return JSON.parse(localStorage.getItem('@discount/discountList') || [])
   } catch (e) {
-    return [];
+    return []
   }
 }
 
-export default App;
+export default App
